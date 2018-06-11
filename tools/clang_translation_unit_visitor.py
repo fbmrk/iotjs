d34b1c1d471fc7744050704247daed676e121201
Gabor Ballabas <gaborb@inf.u-szeged.hu>
2018-06-11 10:39:23 +0000

    Implement pointer resolving

--- tools/clang_translation_unit_visitor.py
+++ tools/clang_translation_unit_visitor.py
@@ -5,8 +5,7 @@
 import clang.cindex
 
 
-
-class ClangASTNode:
+class ClangASTType:
     char_type_kinds = [
         clang.cindex.TypeKind.CHAR_U,
         clang.cindex.TypeKind.UCHAR,
@@ -33,32 +32,80 @@ class ClangASTNode:
         clang.cindex.TypeKind.LONGDOUBLE
     ]
 
+    def __init__(self, clang_type):
+        # We are only interested in the underlying canonical types.
+        self._canonical_type = clang_type.get_canonical()
+
+    @property
+    def type_name(self):
+        return self._canonical_type.spelling
+
+    def is_bool(self):
+        return self._canonical_type.kind == clang.cindex.TypeKind.BOOL
+
+    def is_char(self):
+        return self._canonical_type.kind in ClangASTType.char_type_kinds
+
+    def is_number(self):
+        return self._canonical_type.kind in ClangASTType.number_type_kinds
+
+    def is_enum(self):
+        return self._canonical_type.kind == clang.cindex.TypeKind.ENUM
+
+    def is_void(self):
+        return self._canonical_type.kind == clang.cindex.TypeKind.VOID
+
+    def is_builtin(self):
+        return (self.is_bool() or self.is_char() or self.is_number())
+
+    def is_pointer(self):
+        return self._canonical_type.kind == clang.cindex.TypeKind.POINTER
+
+    def is_array(self):
+        return (self._canonical_type.kind == clang.cindex.TypeKind.CONSTANTARRAY or
+                self._canonical_type.kind == clang.cindex.TypeKind.INCOMPLETEARRAY)
+
+    # Resolves all array dimensions recursively and returns with the underlying element type.
+    @staticmethod
+    def visit_array(clang_ast_type):
+        if clang_ast_type.is_array():
+            element_type = ClangASTType(clang_ast_type._canonical_type.element_type)
+            return ClangASTType.visit_array(element_type)
+        else:
+            return clang_ast_type
+
+    def get_array_type(self):
+        assert self.is_array()
+
+        return ClangASTType.visit_array(self)
+
+    # Resolves all pointers recursively and returns with the underlying final pointee.
+    @staticmethod
+    def visit_pointer(clang_ast_type):
+        if clang_ast_type.is_pointer():
+            pointee_type = ClangASTType(clang_ast_type._canonical_type.get_pointee())
+            return ClangASTType.visit_pointer(pointee_type)
+        else:
+            return clang_ast_type
+
+    def get_pointee_type(self):
+        assert self.is_pointer()
+
+        return ClangASTType.visit_pointer(self)
+
+
+class ClangASTNode:
     def __init__(self, cursor):
         self._cursor = cursor
-        self._cursor_type_kind = cursor.type.get_canonical().kind
+        self._type = ClangASTType(cursor.type)
 
     @property
     def name(self):
         return self._cursor.spelling
 
     @property
-    def canonical_type_name(self):
-        return self._cursor.type.get_canonical().spelling
-
-    def is_bool(self):
-        return self._cursor_type_kind == clang.cindex.TypeKind.BOOL
-
-    def is_char(self):
-        return self._cursor_type_kind in ClangASTNode.char_type_kinds
-
-    def is_number(self):
-        return self._cursor_type_kind in ClangASTNode.number_type_kinds
-
-    def is_enum(self):
-        return self._cursor_type_kind == clang.cindex.TypeKind.ENUM
-
-    def is_builtin(self):
-        return (self.is_bool() or self.is_char() or self.is_number())
+    def node_type(self):
+        return self._type
 
 
 class ClangEnumConstantDecl(ClangASTNode):
@@ -69,36 +116,21 @@ class ClangEnumConstantDecl(ClangASTNode):
 class ClangFunctionDecl(ClangASTNode):
     def __init__(self, cursor):
         ClangASTNode.__init__(self, cursor)
-        self._parm_decls = []
-        self._canonical_return_type = cursor.type.get_result().get_canonical()
+        self._return_type = ClangASTType(cursor.type.get_result())
 
+        self._parm_decls = []
         function_children = list(cursor.get_children())
         for child in function_children:
             if child.kind == clang.cindex.CursorKind.PARM_DECL:
                 self._parm_decls.append(ClangFunctionParameterDecl(child))
 
     @property
-    def canonical_return_type_name(self):
-        return self._canonical_return_type.spelling
+    def return_type(self):
+        return self._return_type
 
     @property
     def params(self):
         return self._parm_decls
-
-    def is_return_type_enum(self):
-        return self._canonical_return_type.kind == clang.cindex.TypeKind.ENUM
-
-    def is_return_type_void(self):
-        return self._canonical_return_type.kind == clang.cindex.TypeKind.VOID
-
-    def is_return_type_bool(self):
-        return self._canonical_return_type.kind == clang.cindex.TypeKind.BOOL
-
-    def is_return_type_char(self):
-        return self._canonical_return_type.kind in ClangASTNode.char_type_kinds
-
-    def is_return_type_number(self):
-        return self._canonical_return_type.kind in ClangASTNode.number_type_kinds
 
 
 class ClangFunctionParameterDecl(ClangASTNode):
